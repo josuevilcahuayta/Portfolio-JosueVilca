e9918c23c4ba360d2708290d60be658a17a211a7
Eduardo <espejismo_564@hotmail.com>
2025-11-21 18:36:14 -0500

    primer commit incio y proyectos agregados

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./components/header/header";
+import { Proyects } from "./components/proyects/proyects";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Proyects],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
